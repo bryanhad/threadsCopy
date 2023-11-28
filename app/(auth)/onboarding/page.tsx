bfb6df0b8a20ac71b827b655ae4b6a9a40503c64
Bryan Hadinata <bryanhadinata76@gmail.com>
2023-11-28 13:01:59 +0700

    added authentication feature with clerk! so freaking simple.. couldn't believe it my self.
    steps:
    1. install clerk nextjs by running 'npm i @clerk/nextjs'
    2. set env variables needed.
    3. wrap ur app with <ClerkPrivider/>
    4. decide which pages are public and which require auth to access. create a middleware.ts file on ur root file if u're not using src/ dir.
    5. create sign-in and sign-up page, by following the docs. essentially u're going to use clerk's prebuilt components.
    6. add <UserButton/> clerk's component to the home page, so the user can manage their account and logout!

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,9 @@
+async function OnboardingPage() {
+    return (
+        <main>
+            <h1 className="head-text">Onboarding page</h1>
+        </main>
+    )
+}
+
+export default OnboardingPage
